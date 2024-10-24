db3dfca3159889ede617b23aafa3179cf769073e
James <yamezyan@gmail.com>
2024-10-24 16:30:09 -0400

    Copied some text from original page to the about us page

--- src/app/about/page.tsx
+++ src/app/about/page.tsx
@@ -17,6 +17,38 @@ export default function About() {
                 <div id="explanation" className="mb-8 text-lg font-[100]">
                     Fill out the form to request a download link.
                 </div>
+
+                <div id="paragraph" className="mx-60 mb-8 text-xl font-[100] text-left leading-10">
+                    <p> The BOC runs everything from <span className="font-bold">local walks</span> to <span className="font-bold">backpacking weekends, </span>
+                    to <span className="font-bold">local bike rides, </span> to <span className="font-bold">kayaking</span> to <span className="font-bold">day hikes </span> 
+                    to <span className="font-bold">climbing</span> to <span className="font-bold">skiing</span> and everything in between! 
+                    The fall break and spring break <span className="font-bold">backpacking</span> trips are the BOC’s longest trips, going to Adirondack, 
+                    the Blue Ridge, and the White Mountains in recent years. Some of our most popular trips are our short 
+                    walks and outdoor social events. Several subsidized <span className="font-bold">ski trips</span> take place every winter, and local 
+                    <span className="font-bold"> whitewater rafting</span> trips occur in the spring and fall.
+                    </p>
+                </div>
+
+                <div id="paragraph2" className="mx-60 mb-8 text-xl font-[100] text-left leading-10">
+                    <p> Every year, the club offers classes in climbing and backpacking skills. The BOC also runs periodic service 
+                        trips to improve the public’s experience in the outdoors, especially in Rhode Island.
+                    </p>
+                </div>
+
+                <div id="paragraph3" className="mx-60 mb-8 text-xl font-[100] text-left leading-10">
+                    <p> Every year, the club offers classes in climbing and backpacking skills. The BOC also runs periodic service 
+                        trips to improve the public’s experience in the outdoors, especially in Rhode Island.
+                    </p>
+                </div>
+
+                <div id="paragraph4" className="mx-60 mb-8 text-xl font-[100] text-left leading-10">
+                    <p> If you’re interested in the community, challenge, and regeneration that the outdoors offer, join our mailing 
+                        list that includes over 3,000 members of the Brown community—faculty, graduate students, and undergraduate 
+                        students included. If you’d like to support our mission of getting more students outside, click here to 
+                        donate to the BOC!
+                    </p>
+                </div>
+
             </div>
             
             <div className="flex justify-center">
