837c8873bd665dbd86e6af92f7d2c47182d59033
omaro <omar_dixon@brown.edu>
2024-11-20 22:57:11 -0500

    social media revised

--- src/app/components/SocialMedia.tsx
+++ src/app/components/SocialMedia.tsx
@@ -1,17 +1,31 @@
 import Image from 'next/image';
 
 const SocialMedia: React.FC = () => {
+    const socialLinks = [
+        {
+            href: "https://www.facebook.com/brownoutingclub/",
+            src: "/images/social/facebook.png",
+            alt: "Facebook",
+        },
+        {
+            href: "https://www.instagram.com/brownoutingclub/?hl=en",
+            src: "/images/social/instagram.png",
+            alt: "Instagram",
+        },
+        {
+            href: "https://www.twitter.com",
+            src: "/images/social/twitter.png",
+            alt: "Twitter",
+        },
+    ];
+
     return (
         <div style={{ display: 'flex', justifyContent: 'flex-start', marginTop: '10px', width: '100%', marginLeft: '-20px' }}>
-            <a href="https://www.facebook.com/brownoutingclub/" target="_blank" rel="noopener noreferrer">
-                <Image src="/images/social/facebook.png" alt="Facebook" width={40} height={40} style={{ margin: '0 15px' }} />
-            </a>
-            <a href="https://www.instagram.com/brownoutingclub/?hl=en" target="_blank" rel="noopener noreferrer">
-                <Image src="/images/social/instagram.png" alt="Instagram" width={40} height={40} style={{ margin: '0 15px' }} />
-            </a>
-            <a href="https://www.twitter.com" target="_blank" rel="noopener noreferrer">
-                <Image src="/images/social/twitter.png" alt="Twitter" width={40} height={40} style={{ margin: '0 15px' }} />
-            </a>
+            {socialLinks.map(({ href, src, alt }, index) => (
+                <a key={index} href={href} target="_blank" rel="noopener noreferrer" style={{ margin: '0 15px' }}>
+                    <Image src={src} alt={alt} width={40} height={40} />
+                </a>
+            ))}
         </div>
     );
 };
