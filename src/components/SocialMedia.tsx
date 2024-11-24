0599ca5b946631d27de28a278c327dbebdda5271
Alan W <awangc31s@gmail.com>
2024-11-23 21:47:23 -0500

    Merged with overhaul

--- src/components/SocialMedia.tsx
+++ src/components/SocialMedia.tsx
@@ -1,7 +1,8 @@
-import Facebook from "@/assets/images/facebook.png";
 import Instagram from "@/assets/images/instagram.png";
 import Twitter from "@/assets/images/twitter.png";
+import React from 'react';
 import Image from 'next/image';
+
 
 const SocialMedia: React.FC = () => {
     const socialLinks = [
@@ -9,24 +10,53 @@ const SocialMedia: React.FC = () => {
             href: "https://www.facebook.com/brownoutingclub/",
             src: Facebook.src,
             alt: "Facebook",
+            label: "Visit our Facebook page",
         },
         {
             href: "https://www.instagram.com/brownoutingclub/?hl=en",
             src: Instagram.src,
             alt: "Instagram",
+            label: "Visit our Instagram profile",
         },
         {
             href: "https://www.twitter.com",
             src: Twitter.src,
             alt: "Twitter",
+            label: "Visit our Twitter page",
         },
     ];
 
+    const containerStyles = {
+        display: 'flex',
+        justifyContent: 'flex-start',
+        marginTop: '10px',
+        width: '100%',
+        marginLeft: '-5%',
+    };
+
+    const linkStyles = {
+        margin: '0 3%',
+        display: 'inline-block',
+    };
+
     return (
-        <div style={{ display: 'flex', justifyContent: 'flex-start', marginTop: '10px', width: '100%', marginLeft: '-20px' }}>
-            {socialLinks.map(({ href, src, alt }, index) => (
-                <a key={index} href={href} target="_blank" rel="noopener noreferrer" style={{ margin: '0 15px' }}>
-                    <Image src={src} alt={alt} width={40} height={40} />
+        <div style={containerStyles}>
+            {socialLinks.map(({ href, src, alt, label }, index) => (
+                <a
+                    key={index}
+                    href={href}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    aria-label={label}
+                    style={linkStyles}
+                >
+                    <Image 
+                        src={src} 
+                        alt={alt} 
+                        width={40} 
+                        height={40} 
+                        priority 
+                    />
                 </a>
             ))}
         </div>
@@ -34,6 +64,7 @@ const SocialMedia: React.FC = () => {
 };
 
 export default SocialMedia;
+
 
 
 
